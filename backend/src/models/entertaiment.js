c569865b9b4fb596f9caaa91a46afe4350534f5e
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-04-24 15:49:44 +0400

    feat(models): feature entertaiment

---
+++ backend/src/models/entertaiment.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Заглушки пока не определимся с составом информации
+let attachmentSchema = new Schema({ //Прикрепленные файлы
+    name: String,
+    path: String
+})
+let costSchema = new Schema({
+    value: mongoose.Decimal128,
+    currency: String
+})
+/*
+Основные данные по развлечениям: тип, компания, где, когда(начало/конец).
+*/
+const entertaimentCardSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    travelers: [String], //same as traveler.id type
+    payer: {
+        type: String, // traveler.id who payed
+    },
+    cost: costSchema,
+    type: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    company: {
+        type: String,
+    },
+    place: {
+        type: String,
+    },
+    beginDate: {
+        type: String,
+    },
+    endDate: {
+        type: String,
+    },
+    comment: {
+        type: String,
+    },
+    attachments: [attachmentSchema],
+
+});
+
+module.exports = mongoose.model("entertaimentCard", entertaimentCardSchema);
