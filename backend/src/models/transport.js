7171a382806334ef027af765ab1c82d46abe17d4
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-04-24 15:34:26 +0400

    feat(models): feature transport

---
+++ backend/src/models/transport.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Заглушки пока не определимся с составом информации
+let attachmentSchema = new Schema({ //Прикрепленные файлы
+    name: String
+})
+let costSchema = new Schema({
+    value: mongoose.Decimal128,
+    currency: String
+})
+/*
+Основные данные путешественника, его контакты и доски
+*/
+const transportCardSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    travelers: [String], //same as traveler.id type
+    payer: {
+        type: String, // traveler.id who payed
+    },
+    cost: costSchema,
+    transport: {
+        type: String,
+    },
+    company: {
+        type: String,
+    },
+    departurePlace: {
+        type: String,
+    },
+    departureDate: {
+        type: String,
+    },
+    arrivalPlace: {
+        type: String,
+    },
+    arrivalDate: {
+        type: String,
+    },
+    comment: {
+        type: String,
+    },
+    attachments: [attachmentSchema],
+
+});
+
+module.exports = mongoose.model("transportCard", transportCardSchema);
