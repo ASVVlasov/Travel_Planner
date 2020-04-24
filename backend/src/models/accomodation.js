ce56889a54f71aa015acce4e9fb4d427138317ce
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-04-24 15:42:48 +0400

    feat(models): feature accomodation

---
+++ backend/src/models/accomodation.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Заглушки пока не определимся с составом информации
+let attachmentSchema = new Schema({ //Прикрепленные файлы
+    name: String
+})
+let costSchema = new Schema({
+    value: mongoose.Decimal128,
+    currency: String
+})
+/*
+Основные данные по размещению: тип, название, компания, въезд/выезд.
+*/
+const accomodationCardSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    travelers: [String], //same as traveler.id type
+    payer: {
+        type: String, // traveler.id who payed
+    },
+    cost: costSchema,
+    type: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    company: {
+        type: String,
+    },
+    checkIn: {
+        type: String,
+    },
+    checkOut: {
+        type: String,
+    },
+    comment: {
+        type: String,
+    },
+    attachments: [attachmentSchema],
+
+});
+
+module.exports = mongoose.model("accomodationCard", accomodationCardSchema);
