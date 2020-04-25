59a9381308a01cf937cef3835854d0354d033ff4
ASVVlasov <asvvlasov@gmail.com>
2020-04-25 14:21:34 +0300

    build(project): test to deploy with common package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,11 +20,12 @@ mongoose.connection.once('open', () => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/', express.static(path.resolve(__dirname, '..', '..', 'frontend', 'build')));
 
 const router = require(path.resolve(__dirname, '.', 'routes'));
 
 app.use(router);
 
 app.listen(PORT, () => {
-    console.log('server has been started');
+    console.log(`server has been started at http://localhost:${PORT}`);
 });
