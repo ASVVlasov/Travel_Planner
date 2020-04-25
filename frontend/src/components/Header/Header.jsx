e65f11be911ce92053ba5fd4745ee93eecf19de4
rri9 <rri9@mail.ru>
2020-04-25 22:34:23 +0300

    feat(header): add html

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,10 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import connect from 'react-redux/es/connect/connect';
 
-
+import styles from './Header.module.scss';
+import backBtnPic from './images/back-btn.svg';
+import editBtnPic from './images/edit-pencil.svg';
+import userProfilePic from './images/user-profile.svg';
 
 class Header extends React.Component {
   constructor(props) {
@@ -16,8 +19,55 @@ class Header extends React.Component {
 
   render() {
     return (
-    <header className="header">
-        This is header!
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles["header-row"]}>
+          <div className={styles["back-btn"]}>
+            <img src={backBtnPic} alt="back-btn" />
+          </div>
+          <div className={styles["trip-data"]}>
+            <div className={styles["trip-data__item"]}>
+              <div className={styles["trip-data__title-text"]}>
+                Евротур’2020
+              </div>
+              <div className={styles["trip-data__edit-btn"]}>
+                <img src={editBtnPic} alt="edit-btn" />
+              </div>
+            </div>
+            <div className={styles["trip-data__item"]}>
+              <div className={styles["trip-data__period-text"]}>
+                24 июня, ПТ – 10 июля, ВС  |  14 дней
+              </div>
+              <div className={styles["trip-data__edit-btn"]}>
+                <img src={editBtnPic} alt="edit-btn" />
+              </div>
+            </div>
+          </div>
+
+          <div className={styles["travellers"]}>
+            <div className={styles["travellers-item"]}>
+              <img src="" alt="user-pic" />
+            </div>
+            <div className={styles["travellers-item"]}>
+              <img src="" alt="user-pic" />
+            </div>
+            <div className={styles["travellers-item"]}>
+              <img src="" alt="user-pic" />
+            </div>
+            <div className={styles["travellers-item"]}>
+              <img src="" alt="user-pic" />
+            </div>
+            <div className={styles["travellers-else"]}>
+              <span>+2</span>
+            </div>
+          </div>
+          
+          <div className={styles["userProfile"]}>
+            <img src={userProfilePic} alt="user-profile" />
+          </div>
+
+        </div>
+      </div>  
     </header>
     )
   }
