c123f472cb5882d55ba2c5de74cd6cf1ca90651e
Maria Vatolina <mary4erry@yandex.ru>
2020-04-26 14:05:22 +0300

    feat(TravelPage):add html

--- frontend/src/pages/TravelPage/TravelPage.jsx
+++ frontend/src/pages/TravelPage/TravelPage.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 
-import './TravelPage.module.css'
+import { bindActionCreators } from 'redux';
+import connect from 'react-redux/es/connect/connect';
 
-class Layout extends React.Component {
+import styles from './TravelPage.module.scss'
+
+class TravelPage extends React.Component {
     render() {
-
         return (
-            <div className="container">
-                <Header />
-                <Board />
-                <SideBar />
-                <Footer />
-
+            <div className={styles.container}>
+                <div className={styles.header}></div>
+                <div className={styles.board}></div>
+                <div className={styles["side-bar"]}></div>
+                <div className={styles.footer}></div>
             </div>
         )
     }
 }
+
+const mapStateToProps = ({ }) => ({ })
+const mapDespatchToProps = dispatch => bindActionCreators( { }, dispatch)
+
+export default connect(mapStateToProps, mapDespatchToProps)(TravelPage) 
