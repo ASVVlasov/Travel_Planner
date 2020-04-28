0f8533bd7cfc693ce3523d3714002d418b810565
Discipulus101 <Discipulus101@users.noreply.github.com>
2020-04-28 23:43:10 +0300

    feat(TravelPage/Sidebar): add styles and icon

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import styles from './Sidebar.module.scss'
 
-import { ReactComponent as TransportBtnSVG } from './images/transportBtn.svg';
-import { ReactComponent as AccomodationBtnSVG } from './images/accomodationBtn.svg';
-import { ReactComponent as EntertainmentBtnSVG } from './images/entertainmentBtn.svg';
-import { ReactComponent as ToDoBtnSVG } from './images/toDoBtn.svg';
+import { ReactComponent as TransportBtnSVG } from '../../assets/images/icons/plane.svg';
+import { ReactComponent as AccomodationBtnSVG } from '../../assets/images/icons/house.svg';
+import { ReactComponent as EntertainmentBtnSVG } from '../../assets/images/icons/theater.svg';
+import { ReactComponent as ToDoBtnSVG } from '../../assets/images/icons/clipboard.svg';
 
 export default class Sidebar extends React.Component {
     render() {
         return (
             <div className={styles.sidebar}>
                 <div className={styles.sidebar__wrapperTransportBtnSVG}>
-                    <a href="#"><TransportBtnSVG/></a>
+                    <a className={`${styles.sidebar__link} ${styles.sidebar__link_active}`} href="#"><TransportBtnSVG/></a>
                 </div>
                 <div className={styles.sidebar__wrapperAccomodationBtnSVG}>
-                    <a href="#"><AccomodationBtnSVG/></a>
+                    <a className={styles.sidebar__link} href="#"><AccomodationBtnSVG/></a>
                 </div>
                 <div className={styles.sidebar__wrapperEntertainmentBtnSVG}>
-                    <a href="#"><EntertainmentBtnSVG/></a>
+                    <a className={styles.sidebar__link} href="#"><EntertainmentBtnSVG/></a>
                 </div>
                 <div className={styles.sidebar__wrapperEntertainmentBtnSVG}>
-                    <a href="#"><ToDoBtnSVG/></a>
+                    <a className={styles.sidebar__link} href="#"><ToDoBtnSVG/></a>
                 </div>
             </div>
         )
