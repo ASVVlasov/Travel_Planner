3fe952254058dca6549eb22e2b8f2e842ffa0e2a
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-05-01 17:55:06 +0400

    fix(controllers): Переделал проверки запроса под универсальный формат

--- backend/src/controllers/requestCheck.js
+++ backend/src/controllers/requestCheck.js
@@ -1,35 +1,33 @@
 const Traveler = require("../models/traveler.js");
 
-const travelerID = (req, res) => {
-    if (!req.body.travelerID) {
+const haveID = (id, res) => {
+    if (!id) {
         res.status(400).json({
-            status: "Request error: empty travelerID",
+            status: "Request error: empty ID",
         });
         return false
     } else return true
 };
 
-const travelerExists = async (req, res) => {
-    if (await Traveler.findById(req.body.travelerID) === null) {
+const recordExists = async (res, id, Model) => {
+    if (await Model.findById(id) === null) {
         res.status(400).json({
             status: "Request error: wrong id",
         })
         return false
     } else return true
 }
-const travelerCanUpdate = (req, res) => {
-    if (Object.keys(req.body.traveler).length == 0) {
+const canUpdate = (updatedFields, res) => {
+    if (Object.keys(updatedFields).length == 0) {
         res.status(400).json({
-            status: "nothing to update"
+            status: "Request error: nothing to update"
         })
         return false
     } else return true
-
-
 }
 
 module.exports = {
-    travelerID,
-    travelerExists,
-    travelerCanUpdate
+    haveID,
+    recordExists,
+    canUpdate
 }
