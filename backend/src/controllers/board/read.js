0ae9cd819e2a43d0a532d20ca471132dda72dafe
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-05-01 18:29:27 +0400

    feat(controllers): board read

---
+++ backend/src/controllers/board/read.js
@@ -0,0 +1,23 @@
+const Board = require("../../models/board.js");
+const ErrorHandler = require("../errorHandler.js")
+const Request = require("../requestCheck.js")
+
+const read = async (req, res) => {
+    if (!Request.haveID(req.body.boardID)) {
+        ErrorHandler.emptyID(req, res, "board");
+        return;
+    }
+    if (!Request.recordExists(req.body.boardID, Board)) {
+        ErrorHandler.wrongID(req, res, "traveler");
+        return;
+    }
+    try {
+        let board = await Traveler.findById(req.body.boardID);
+        if (board === null) throw (err)
+        res.json(board);
+    } catch (err) {
+        ErrorHandler.readError(req, res, err)
+    }
+};
+
+module.exports = read;
