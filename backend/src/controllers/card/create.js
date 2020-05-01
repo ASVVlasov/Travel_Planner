dcf2e5192b8a63a2525ec7ec5f7dd3065641c545
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-05-01 19:33:12 +0400

    feat(controllers): card create

---
+++ backend/src/controllers/card/create.js
@@ -0,0 +1,51 @@
+const Board = require("../../models/board.js");
+const Accomodation = require("../../models/accomodation.js");
+const Entertaiment = require("../../models/entertaiment.js");
+const Transport = require("../../models/transport.js");
+const Todo = require("../../models/todo.js");
+
+const ErrorHandler = require("../errorHandler.js")
+const Request = require("../requestCheck.js")
+
+const create = async (req, res) => {
+    if (!Request.haveID(req.body.boardID)) {
+        ErrorHandler.emptyID(req, res, "board");
+        return;
+    }
+    if (!(await Request.recordExists(req.body.boardID, Board))) {
+        ErrorHandler.wrongID(req, res, "board");
+        return;
+    }
+    if (!Request.haveType(req.body.cardType)) {
+        ErrorHandler.emptyID(req, res, req.body.cardType);
+        return;
+    }
+    try {
+        let board = await Board.findById(boardID)
+        let newCard;
+        switch (req.body.cardType) {
+            case "transport":
+                newCard = new Transport(req.body.card)
+                board.transportCards.push(newCard)
+                break;
+            case "entertaiment":
+                newCard = new Entertaiment(req.body.card)
+                board.entertaimentCards.push(newCard)
+                break;
+            case "accomodation":
+                newCard = new Accomodation(req.body.card)
+                board.accomodationCards.push(newCard)
+                break;
+            case "todo":
+                newCard = new Todo(req.body.card)
+                board.todoCards.push(newCard)
+                break;
+        }
+        await board.save()
+        res.json(newCard);
+    } catch (err) {
+        ErrorHandler.createError(req, res, err)
+    }
+};
+
+module.exports = create;
