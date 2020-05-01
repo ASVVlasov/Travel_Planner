c15ff7d9e88d7ddf546b56757c7842eef115321c
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-05-01 18:24:14 +0400

    fix(controllers): traveler delete

--- backend/src/controllers/traveler/delete.js
+++ backend/src/controllers/traveler/delete.js
@@ -3,12 +3,18 @@ const ErrorHandler = require("../errorHandler.js")
 const Request = require("../requestCheck.js")
 
 const destroy = async (req, res) => {
-    if (!(Request.haveID(req.body.travelerID, res) &&
-            await Request.recordExists(res, req.body.travelerID, Traveler))) return
+    if (!Request.haveID(req.body.travelerID)) {
+        ErrorHandler.emptyID(req, res, "traveler");
+        return;
+    }
+    if (!Request.recordExists(req.body.travelerID, Traveler)) {
+        ErrorHandler.wrongID(req, res, "traveler");
+        return;
+    }
     try {
         let deletedTraveler = await Traveler.findById(id)
         await Traveler.deleteOne({
-            _id: id
+            _id: req.body.travelerID
         })
         res.status(200).json({
             status: `Traveler ${deletedTraveler.login} deleted`
