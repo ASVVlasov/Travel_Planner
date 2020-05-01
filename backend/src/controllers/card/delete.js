50700a18017d31fac8f3c9b949b39e83a20bab3c
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-05-01 19:59:51 +0400

    feat(controllers): card delete

---
+++ backend/src/controllers/card/delete.js
@@ -0,0 +1,44 @@
+const Board = require("../../models/board.js");
+
+const ErrorHandler = require("../errorHandler.js")
+const Request = require("../requestCheck.js")
+
+const destroy = async (req, res) => {
+    if (!Request.haveID(req.body.boardID)) {
+        ErrorHandler.emptyID(req, res, "board");
+        return;
+    }
+    if (!(await Request.recordExists(req.body.boardID, Board))) {
+        ErrorHandler.wrongID(req, res, "board");
+        return;
+    }
+    if (!Request.haveType(req.body.cardType)) {
+        ErrorHandler.emptyID(req, res, req.body.cardType);
+        return;
+    }
+    if (!Request.typeExists(req.body.cardType)) {
+        ErrorHandler.wrongType(req, res, req.body.cardType)
+    }
+    if (!Request.haveID(req.body.cardID)) {
+        ErrorHandler.emptyID(req, res, "card");
+        return;
+    }
+    try {
+        let board = await Board.findById(boardID)
+        let card = board[req.body.cardType + "Cards"].id(req.body.cardID);
+        if (card === null) throw (err)
+        res.json(card);
+
+        let deletedCard = board[req.body.cardType + "Cards"].id(req.body.cardID);
+        await board[req.body.cardType + "Cards"].id(req.body.cardID).remove();
+        await board.save();
+        res.status(200).json({
+            status: `AccomodationCard ${deletedCard._id} deleted`
+        })
+
+    } catch (err) {
+        ErrorHandler.readError(req, res, err)
+    }
+};
+
+module.exports = destroy;
