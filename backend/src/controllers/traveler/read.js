ed0d6c8a9dcd9c81afd86002dd48aa8fafacc46c
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-05-01 17:57:18 +0400

    fix(controllers): traveler read fix after requestCheck update

--- backend/src/controllers/traveler/read.js
+++ backend/src/controllers/traveler/read.js
@@ -1,9 +1,9 @@
 const Traveler = require("../../models/traveler.js");
 const ErrorHandler = require("../errorHandler.js")
-const RequestCheck = require("../requestCheck.js")
+const Request = require("../requestCheck.js")
 
 const read = async (req, res) => {
-    if (!RequestCheck.travelerID(req, res)) return;
+    if (!Request.haveID(req.body.travelerID, res)) return;
     try {
         let traveler = await Traveler.findById(req.body.travelerID);
         if (traveler === null) throw (err)
