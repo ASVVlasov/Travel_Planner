5ca053e9ed926dcc4e0b47606c22232ce9c5f7a8
rri9 <rri9@mail.ru>
2020-05-03 17:11:21 +0300

    feat(routing): add basic routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import './index.scss';
 import { Provider } from 'react-redux';
 import initStore, { history } from './redux/store.js';
 import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import TravelPage from "./pages/TravelPage/TravelPage";
 
@@ -13,7 +14,13 @@ const store = initStore();
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history} >
-      <TravelPage />
+      <Switch>
+        <Route exact path='/cabinet' render={() => <h1>User page will be here soon!</h1>} />
+        <Route path='/travel' component={TravelPage} />
+        <Route path="/"> {/* // FIX Temporary route */}
+          <Redirect to="/travel" />
+        </Route>
+      </Switch>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
--- frontend/src/pages/TravelPage/TravelPage.jsx
+++ frontend/src/pages/TravelPage/TravelPage.jsx
@@ -1,30 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 import { bindActionCreators } from 'redux';
 import connect from 'react-redux/es/connect/connect';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
-import styles from './TravelPage.module.scss'
-import Header from "../../components/Header/Header";
-import Board from "../../components/Board/Board";
-import Footer from "../../components/Footer/Footer";
-import Sidebar from "../../components/Sidebar/Sidebar";
+import styles from './TravelPage.module.scss';
+import Header from '../../components/Header/Header';
+import Board from '../../components/Board/Board';
+import Footer from '../../components/Footer/Footer';
+import Sidebar from '../../components/Sidebar/Sidebar';
 
 class TravelPage extends React.Component {
-    render() {
-        return (
-            <div className={styles.travelPage}>
-                <Header />
-                <Board />
-                <Footer />
-                <div className={styles.travelPage__sidebarWrap}>
-                    <Sidebar />
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { url } = this.props.match;
+    return (
+      <div className={styles.travelPage}>
+        <Header />
+        <Switch>
+          <Route path={`${url}/:tab`} component={Board} />
+          <Route>
+            <Redirect to={`${url}/transport`} /> {/* Default tab is transport */}
+          </Route>
+        </Switch>
+        <Footer />
+        <div className={styles.travelPage__sidebarWrap}>
+          <Sidebar />
+        </div>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = ({ }) => ({ })
-const mapDispatchToProps = dispatch => bindActionCreators( { }, dispatch)
+const mapStateToProps = ({ }) => ({ });
+const mapDispatchToProps = (dispatch) => bindActionCreators({ }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(TravelPage)
+export default connect(mapStateToProps, mapDispatchToProps)(TravelPage);
