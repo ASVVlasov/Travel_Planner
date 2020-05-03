a78d00db7d43627ba438a336aad5f11b0f30b8c1
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-05-03 23:15:22 +0400

    feat(routes): add mongoose models

--- backend/src/routes/swagger.js
+++ backend/src/routes/swagger.js
@@ -1,15 +1,32 @@
-const mongoose = require('mongoose');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger/index.json');
-const m2s = require('mongoose-to-swagger');
-const router = require('express').Router();
+const mongoose = require('mongoose')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../swagger/index.json')
+const m2s = require('mongoose-to-swagger')
+const router = require('express').Router()
 
-const Traveler = require("../models/traveler");
-const TravelerSchema = m2s(Traveler);
-const header = require("../swagger/header.json");
-header.components.schemas.Traveler = TravelerSchema;
+const Traveler = require("../models/traveler.js")
+const Board = require("../models/board.js")
+const Accomodation = require("../models/accomodation.js")
+const Entertaiment = require("../models/entertaiment.js")
+const Transport = require("../models/transport.js")
+const Todo = require("../models/todo.js")
+const Attachment = require("../models/misc/attachment.js")
+const TravelerSchema = m2s(Traveler)
+const BoardSchema = m2s(Board)
+const AccomodationSchema = m2s(Accomodation)
+const EntertaimentSchema = m2s(Entertaiment)
+const TransportSchema = m2s(Transport)
+const TodoSchema = m2s(Todo)
+const AttachmentSchema = m2s(Attachment)
+const header = require("../swagger/header.json")
+header.components.schemas.Traveler = TravelerSchema
+header.components.schemas.Board = BoardSchema
+header.components.schemas.Accomodation = AccomodationSchema
+header.components.schemas.Entertaiment = EntertaimentSchema
+header.components.schemas.Transport = TransportSchema
+header.components.schemas.Todo = TodoSchema
+header.components.schemas.Attachment = AttachmentSchema
+router.use('/', swaggerUi.serve)
+router.get('/', swaggerUi.setup(header))
 
-router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(header));
-
-module.exports = router;
+module.exports = router
