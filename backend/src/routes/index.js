512a41f2deaa5dc7c39d989235ed3be3eed651bb
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-05-03 18:24:33 +0400

    feat(swagger): init commit
    Подключен со страницей по-умолчанию по роуту "/api-docs/

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger/index.json');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 
 module.exports = router;
