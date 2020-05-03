2690fa23c28b248ddacc9bf08c06b573e198d72f
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-05-03 23:50:51 +0400

    feat(swagger): migrated schemas to schemas.js

--- backend/src/routes/swagger.js
+++ backend/src/routes/swagger.js
@@ -1,32 +1,9 @@
-const mongoose = require('mongoose')
 const swaggerUi = require('swagger-ui-express')
-const m2s = require('mongoose-to-swagger')
 const router = require('express').Router()
 
-const Traveler = require("../models/traveler.js")
-const Board = require("../models/board.js")
-const Accomodation = require("../models/accomodation.js")
-const Entertaiment = require("../models/entertaiment.js")
-const Transport = require("../models/transport.js")
-const Todo = require("../models/todo.js")
-const Attachment = require("../models/misc/attachment.js")
-const TravelerSchema = m2s(Traveler)
-const BoardSchema = m2s(Board)
-const AccomodationSchema = m2s(Accomodation)
-const EntertaimentSchema = m2s(Entertaiment)
-const TransportSchema = m2s(Transport)
-const TodoSchema = m2s(Todo)
-const AttachmentSchema = m2s(Attachment)
 const document = require("../swagger/definition.js")
 document.components = {}
-document.components.schemas = {}
-document.components.schemas.Traveler = TravelerSchema
-document.components.schemas.Board = BoardSchema
-document.components.schemas.Accomodation = AccomodationSchema
-document.components.schemas.Entertaiment = EntertaimentSchema
-document.components.schemas.Transport = TransportSchema
-document.components.schemas.Todo = TodoSchema
-document.components.schemas.Attachment = AttachmentSchema
+document.components.schemas = require("../swagger/schemas.js")
 router.use('/', swaggerUi.serve)
 router.get('/', swaggerUi.setup(document))
 
---
+++ backend/src/swagger/schemas.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const m2s = require('mongoose-to-swagger')
+/* Models */
+const Traveler = require("../models/traveler.js")
+const Board = require("../models/board.js")
+const Accomodation = require("../models/accomodation.js")
+const Entertaiment = require("../models/entertaiment.js")
+const Transport = require("../models/transport.js")
+const Todo = require("../models/todo.js")
+const Attachment = require("../models/misc/attachment.js")
+/* Documents */
+const TravelerSchema = m2s(Traveler)
+const BoardSchema = m2s(Board)
+const AccomodationSchema = m2s(Accomodation)
+const EntertaimentSchema = m2s(Entertaiment)
+const TransportSchema = m2s(Transport)
+const TodoSchema = m2s(Todo)
+const AttachmentSchema = m2s(Attachment)
+/* Combining schema documents in one */
+const schemas = {}
+schemas.Traveler = TravelerSchema
+schemas.Board = BoardSchema
+schemas.Accomodation = AccomodationSchema
+schemas.Entertaiment = EntertaimentSchema
+schemas.Transport = TransportSchema
+schemas.Todo = TodoSchema
+schemas.Attachment = AttachmentSchema
+
+module.exports = schemas
