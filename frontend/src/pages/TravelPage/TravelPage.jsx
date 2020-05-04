29b93561aea2cb7d245b93a8b0f405eda80032a5
Elena Maltseva <lena@maltseva.design>
2020-05-04 23:44:41 +0300

    feat(TravelPage): edit styles after tests

--- frontend/src/pages/TravelPage/TravelPage.jsx
+++ frontend/src/pages/TravelPage/TravelPage.jsx
@@ -1,30 +1,34 @@
 import React from 'react'
 
-import { bindActionCreators } from 'redux';
-import connect from 'react-redux/es/connect/connect';
+import { bindActionCreators } from 'redux'
+import connect from 'react-redux/es/connect/connect'
 
 import styles from './TravelPage.module.scss'
-import Header from "../../components/Header/Header";
-import Board from "../../components/Board/Board";
-import Footer from "../../components/Footer/Footer";
-import Sidebar from "../../components/Sidebar/Sidebar";
+import Header from '../../components/Header/Header'
+import Board from '../../components/Board/Board'
+import Footer from '../../components/Footer/Footer'
+import Sidebar from '../../components/Sidebar/Sidebar'
 
 class TravelPage extends React.Component {
-    render() {
-        return (
-            <div className={styles.travelPage}>
-                <Header />
-                <Board />
-                <Footer />
-                <div className={styles.travelPage__sidebarWrap}>
-                    <Sidebar />
-                </div>
+   render() {
+      return (
+         <div className={styles.travelPage}>
+            <Header />
+            <div className={styles.travelPage__boardWrap}>
+               <Board />
             </div>
-        )
-    }
+            <div className={styles.travelPage__footerWrap}>
+               <Footer />
+            </div>
+            <div className={styles.travelPage__sidebarWrap}>
+               <Sidebar />
+            </div>
+         </div>
+      )
+   }
 }
 
-const mapStateToProps = ({ }) => ({ })
-const mapDispatchToProps = dispatch => bindActionCreators( { }, dispatch)
+const mapStateToProps = ({}) => ({})
+const mapDispatchToProps = (dispatch) => bindActionCreators({}, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TravelPage)
