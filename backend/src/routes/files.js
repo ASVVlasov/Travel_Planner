e3444a2cd503a5ffda16a885d58594363e0f8e2c
Alexandr <asvvlasov@gmail.com>
2020-05-05 23:19:45 +0300

    fix(files): fix after resolved merge conflicts

--- backend/src/routes/files.js
+++ backend/src/routes/files.js
@@ -1,14 +1,13 @@
 const path = require('path')
 const express = require('express')
 const router = express.Router()
-const attachments = require(path.resolve(
-   __dirname,
-   '..',
-   'controllers',
-   'card',
-   'attachment'
-))
-router.post('/attachFile', attachments.attach)
-router.post('/deattachFile', attachments.deattach)
+const attachments = require(path.resolve(__dirname, '..', 'controllers', 'card', 'attachment'))
+
+router.post('/attachFile', (req, res) => {
+   res.json(attachments.attach(req))
+})
+router.post('/deattachFile', (req, res) => {
+   res.json(attachments.deattach(req))
+})
 router.use('/', express.static(path.resolve(__dirname, '..', 'uploads')))
 module.exports = router
