8742602dd1daf96a10c2d9398d27e0c4806715ef
Maria Vatolina <mary4erry@yandex.ru>
2020-05-06 13:59:00 +0300

    feat(Board): add the ability to show modal window

--- frontend/src/components/Board/Board.jsx
+++ frontend/src/components/Board/Board.jsx
@@ -1,112 +1,127 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames'
-import styles from './Board.module.scss'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import styles from "./Board.module.scss";
 
-import BoardSlider from './BoardSlider'
-import Button from '../../controls/Button/Button'
- 
+import BoardSlider from "./BoardSlider";
+import ButtonAddForm from "../TransportAddForm/TransportAddForm";
+import Button from "../../controls/Button/Button";
+
 export default class Board extends Component {
    static propTypes = {
-      tabs: PropTypes.array,      // TODO add .isRequired after the real data appears
-   }
+      tabs: PropTypes.array, // TODO add .isRequired after the real data appears
+   };
 
-   FAKEprops = {     // TODO remove object after the real data appears
+   FAKEprops = {
+      // TODO remove object after the real data appears
       tabs: [
          {
-            title: 'Транспорт',
-            link: '/',
+            title: "Транспорт",
+            link: "/",
             cards: [
                {
-                  transport: 'card1' 
-               }, 
+                  transport: "card1",
+               },
                {
-                  transport: 'card2' 
-               }, 
+                  transport: "card2",
+               },
                {
-                  transport: 'card3' 
+                  transport: "card3",
                },
             ],
          },
-      ]
-   }
-   
+      ],
+   };
+
    state = {
-      activeTabLink: '',
+      activeTabLink: "",
       tabs: [],
       cards: [],
-   }
+      isModalOpen: false,
+   };
 
    parsePropsToState = () => {
-      const cardsList = []
+      const cardsList = [];
 
-      const tabsList = this.FAKEprops.tabs.map(tab => {     // TODO remove 'FAKE' after the real data appears
-         const { title, link, cards } = tab
-         const activeTab = link === window.location.pathname
+      const tabsList = this.FAKEprops.tabs.map((tab) => {
+         // TODO remove 'FAKE' after the real data appears
+         const { title, link, cards } = tab;
+         const activeTab = link === window.location.pathname;
 
          if (activeTab) {
-            this.setState({ activeTabLink: link }) 
+            this.setState({ activeTabLink: link });
          }
          if (activeTab && cards.length > 0) {
-            cardsList.push(...cards) 
+            cardsList.push(...cards);
          }
 
-         return { title, link }
-      })
+         return { title, link };
+      });
 
-      this.setState({ tabs: tabsList, cards: cardsList })
-   }
+      this.setState({ tabs: tabsList, cards: cardsList });
+   };
 
    mapTabsToRender = () => {
-      return this.state.tabs.map( (tab, index) => {
-         const activeTab = tab.link === this.state.activeTabLink
+      return this.state.tabs.map((tab, index) => {
+         const activeTab = tab.link === this.state.activeTabLink;
          return (
-            <a key={ index }                   // TODO replace with Router(?) later
-               className={ classNames(
-                  styles.board__tabsLink, 
-                  activeTab && styles.board__tabsLink_active,
+            <a
+               key={index} // TODO replace with Router(?) later
+               className={classNames(
+                  styles.board__tabsLink,
+                  activeTab && styles.board__tabsLink_active
                )}
-               href={ tab.link }
-               children={ tab.title }
+               href={tab.link}
+               children={tab.title}
             />
-         )
-      })
-   }
+         );
+      });
+   };
 
    mapCardsToRender = () => {
-      return this.state.cards.map( (card, index) => (
-         <div key={ index }                 // TODO replace with Card component later
-            children={ card.transport }
+      return this.state.cards.map((card, index) => (
+         <div
+            key={index} // TODO replace with Card component later
+            children={card.transport}
          />
-      ))
+      ));
+   };
+
+   componentDidMount() {
+      this.parsePropsToState();
    }
 
-   componentDidMount () {
-      this.parsePropsToState()
-   }
+   openModal = () => {
+      this.setState({ isModalOpen: true });
+   };
 
-   render () {
+   closeModal = () => {
+      this.setState({ isModalOpen: false });
+   };
+
+   render() {
       return (
-         <div className={ styles.board }>
-
-            <div className={ styles.board__controlPanel }>
-               <nav className={ styles.board__tabs }>
-                  { this.mapTabsToRender() }
+         <div className={styles.board}>
+            <div className={styles.board__controlPanel}>
+               <nav className={styles.board__tabs}>
+                  {this.mapTabsToRender()}
                </nav>
 
-               { this.state.cards.length > 2 &&
-                  <Button size="small" text="+"/>
-               }
+               {this.state.cards.length > 2 && (
+                  <Button onClick={this.openModal} size="small" text="+" />
+               )}
             </div>
-
-            <BoardSlider 
-               className={ styles.board__cards } 
-               slides={ [
-                  ...this.mapCardsToRender(), 
-                  <div key={ 1000 }/>       // TODO replace with AddCardButton(?) control later
-               ] }
+            {this.state.isModalOpen && (
+               <ButtonAddForm onClose={this.closeModal} />
+            )}
+            <BoardSlider
+               className={styles.board__cards}
+               slides={[
+                  ...this.mapCardsToRender(),
+                  <div key={1000} />, // TODO replace with AddCardButton(?) control later
+               ]}
             />
          </div>
-      )
+      );
    }
 }
