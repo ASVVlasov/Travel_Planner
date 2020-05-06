91a21594eecf878d97c8f19dc676b958ccad8c45
Elena Maltseva <lena@maltseva.design>
2020-05-06 14:43:47 +0300

    feat(ModalBase):add handler to click outside modal

--- frontend/src/controls/ModalBase/ModalBase.js
+++ frontend/src/controls/ModalBase/ModalBase.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 import styles from './ModalBase.module.scss'
 
 const modalRoot = document.getElementById('modal-root')
 
 export default class ModalBase extends React.Component {
+   static propTypes = {
+      toClose: PropTypes.func,
+   }
+
    el = document.createElement('div')
+   bgClass = styles.background.toString()
 
-   componentDidMount = () => {
-      const background = styles.background.toString()
-      this.el.classList.add(background)
+   handleEvent(e) {
+      if (e.target.classList.value === this.bgClass) {
+         this.props.toClose()
+      }
+   }
 
+   componentDidMount() {
+      this.el.classList.add(this.bgClass)
+      this.el.addEventListener('click', (e) => this.handleEvent(e))
       modalRoot.appendChild(this.el)
    }
-   componentWillUnmount = () => {
+   componentWillUnmount() {
       modalRoot.removeChild(this.el)
    }
    render() {
