6aff4e628e5ae051d8fd781cafa491d1a7a87716
rri9 <rri9@mail.ru>
2020-05-09 06:30:28 +0300

    feat(redux): init

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,23 +1,23 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import { createHashHistory } from 'history';
-import { routerMiddleware } from 'connected-react-router';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { createHashHistory } from 'history'
+import { routerMiddleware } from 'connected-react-router'
 
-import createRootReducer from './reducers';
+import createRootReducer from './reducers'
 
 export const history = createHashHistory({
-  hashType: 'slash',
-});
+   hashType: 'slash',
+})
 
 export default function initStore(preloadedState) {
-  const store = createStore(
-    createRootReducer(history),
-    preloadedState,
-    compose(
-      applyMiddleware(
-        routerMiddleware(history),
-      ),
-    ),
-  );
+   const store = createStore(
+      createRootReducer(history),
+      preloadedState,
+      compose(
+         applyMiddleware(
+            routerMiddleware(history)
+         )
+      )
+   )
 
-  return store;
-};
+   return store
+}
