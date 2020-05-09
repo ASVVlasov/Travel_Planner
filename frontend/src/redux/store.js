e7b04fd0210922121445391303899f24f36fa7ee
rri9 <rri9@mail.ru>
2020-05-09 07:15:30 +0300

    feat(redux): add redux-persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,22 +2,37 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { createHashHistory } from 'history'
 import { routerMiddleware } from 'connected-react-router'
 
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+
 import createRootReducer from './reducers'
+
+const persistConfig = {
+   key: 'TravelPlanner',
+   storage,
+   stateReconciler: autoMergeLevel2
+}
 
 export const history = createHashHistory({
    hashType: 'slash',
 })
 
+const pReducer = persistReducer(persistConfig, createRootReducer(history))
+
 export default function initStore(preloadedState) {
    const store = createStore(
-      createRootReducer(history),
+      pReducer,
       preloadedState,
       compose(
          applyMiddleware(
             routerMiddleware(history)
-         )
+         ),
+         window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
       )
    )
 
-   return store
+   const persistor = persistStore(store)
+
+   return { store, persistor }
 }
