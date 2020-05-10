bb19aa84fb70b4e06ad3efd55f42989f288c6def
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-05-10 18:31:02 +0400

    fix(models) change user nickName field

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
-   nickname: {
+   nickName: {
       type: String,
       required: true,
       description: 'Отображаемое имя (обязательное поле)',
