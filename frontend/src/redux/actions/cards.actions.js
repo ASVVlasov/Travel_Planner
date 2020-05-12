87fb0c9fe8da877db8ac65b455c12ca45b877f3f
Elena Maltseva <lena@maltseva.design>
2020-05-13 01:50:35 +0300

    feat(redux): add actions to delete card

--- frontend/src/redux/actions/cards.actions.js
+++ frontend/src/redux/actions/cards.actions.js
@@ -1,10 +1,32 @@
-import { GET_CARDS } from './types'
+import { GET_CARDS, DELETE_CARD_SUCCESS, DELETE_CARD_ERROR } from './types'
 
-export const getCards = (activeTabId) => {
-   return (dispatch) => dispatch(getCardsList(activeTabId))
-}
-
-const getCardsList = (activeTabId) => ({
+export const getCards = (activeTabId) => ({
    type: GET_CARDS,
    payload: { activeTabId },
+})
+
+export const deleteCard = (travelId, cardId) => {
+   return (dispatch) =>
+      fetch(`/card/${travelId}/${cardId}`, {
+         method: 'DELETE',
+      })
+         .then(
+            (res) => {
+               res.ok
+                  ? dispatch(deleteCardSuccess(cardId))
+                  : dispatch(deleteCardError(res.statusText))
+            },
+            (err) => dispatch(deleteCardError(err))
+         )
+         .catch((err) => dispatch(deleteCardError(err)))
+}
+
+const deleteCardSuccess = (cardId) => ({
+   type: DELETE_CARD_SUCCESS,
+   payload: { cardId },
+})
+
+const deleteCardError = (err) => ({
+   type: DELETE_CARD_ERROR,
+   payload: { err },
 })
--- frontend/src/redux/reducers/board.reducer.js
+++ frontend/src/redux/reducers/board.reducer.js
@@ -1,6 +1,13 @@
-import { GET_BOARD_SUCCESS, GET_BOARD_ERROR, GET_CARDS } from '../actions/types'
+import {
+   GET_BOARD_SUCCESS,
+   GET_BOARD_ERROR,
+   GET_CARDS,
+   DELETE_CARD_SUCCESS,
+   DELETE_CARD_ERROR,
+} from '../actions/types'
 
 const initialState = {
+   travelId: '', // TODO delete after real ID appear in route
    board: [],
    tabs: [],
    cards: [],
@@ -12,7 +19,7 @@ const initialState = {
 export default function boardReducer(state = initialState, action) {
    switch (action.type) {
       case GET_BOARD_SUCCESS: {
-         const { tabs } = action.payload
+         const { _id, tabs } = action.payload
 
          const board = tabs.map((tab) => {
             if (!tab._id) {
@@ -29,22 +36,12 @@ export default function boardReducer(state = initialState, action) {
 
          return {
             ...state,
+            travelId: _id, // TODO delete after real ID appear in route
             board: board,
             tabs: tabsList,
             isLoading: false,
             failureLoading: false,
             errorMessage: '',
-         }
-      }
-
-      case GET_BOARD_ERROR: {
-         return {
-            ...state,
-            tabs: [],
-            cards: [],
-            isLoading: false,
-            failureLoading: true,
-            errorMessage: action.payload.err,
          }
       }
 
@@ -61,6 +58,27 @@ export default function boardReducer(state = initialState, action) {
          })
 
          return { ...state, cards: cardsList }
+      }
+
+      case DELETE_CARD_SUCCESS: {
+         const { cardId } = action.payload
+
+         const cardIndex = state.cards.findIndex((card) => card._id === cardId)
+         state.cards.splice(cardIndex, 1)
+
+         return { ...state }
+      }
+
+      case GET_BOARD_ERROR:
+      case DELETE_CARD_ERROR: {
+         return {
+            ...state,
+            tabs: [],
+            cards: [],
+            isLoading: false,
+            failureLoading: true,
+            errorMessage: action.payload.err,
+         }
       }
 
       default:
