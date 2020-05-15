52ffcdedfaa6c4f31d279e6a244bf6f38ac4112a
Alexandr <asvvlasov@gmail.com>
2020-05-15 13:54:54 +0300

    fix(errorHandler): replace class to object

--- backend/src/models/handlers/errorHandler.js
+++ backend/src/models/handlers/errorHandler.js
@@ -1,12 +1,12 @@
 const createError = require('http-errors')
-class ErrorHandler {
-   static onSave(error, doc, next) {
+const ErrorHandler = {
+   onSave: function (error, doc, next) {
       if (error) {
          next(createError(500, error.errmsg))
       } else {
          next()
       }
-   }
+   },
 }
 
 module.exports = ErrorHandler
