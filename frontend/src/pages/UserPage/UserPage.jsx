e2666997c93b7b3464dc7ccb43ea34a89dc15058
Elena Maltseva <lena@maltseva.design>
2020-05-23 12:37:01 +0300

    feat(UserPage):add check for receive props& Loader

--- frontend/src/pages/UserPage/UserPage.jsx
+++ frontend/src/pages/UserPage/UserPage.jsx
@@ -9,23 +9,38 @@ import styles from './UserPage.module.scss'
 import UserHeader from '../../components/Header/UserHeader'
 import UserBoard from '../../components/Board/UserBoard'
 import UserFooter from '../../components/Footer/UserFooter'
+import { Loader } from '../../controls/Loader/Loader'
 
 class UserPage extends React.Component {
+   isPropsReceived = (object) => Object.keys(object).length
+
    componentDidMount() {
       this.props.getUserInfo()
    }
+
    render() {
+      const { match, user } = this.props
       return (
-         <div className={styles.userPage}>
-            <UserHeader />
-            <Route path={this.props.match.path} component={UserBoard} />
-            <UserFooter />
-         </div>
+         <>
+            {this.isPropsReceived(user) ? (
+               <div className={styles.userPage}>
+                  <UserHeader user={user} />
+                  <Route path={match.path} component={UserBoard} />
+                  <UserFooter />
+               </div>
+            ) : (
+               <Loader />
+            )}
+         </>
       )
    }
 }
 
+const mapStateToProps = ({ userReducer }) => ({
+   user: userReducer,
+})
+
 const mapDispatchToProps = (dispatch) =>
    bindActionCreators({ getUserInfo }, dispatch)
 
-export default connect(null, mapDispatchToProps)(UserPage)
+export default connect(mapStateToProps, mapDispatchToProps)(UserPage)
