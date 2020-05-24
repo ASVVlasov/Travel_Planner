628b862ac2f77b88337d308ee094e0c08932685d
Elena Maltseva <lena@maltseva.design>
2020-05-24 15:39:32 +0300

    feat(redux): add acts&opers for create new travel

--- frontend/src/redux/user/reducer.js
+++ frontend/src/redux/user/reducer.js
@@ -1,4 +1,4 @@
-import { GET_USER_SUCCESS } from '../types'
+import { GET_USER_SUCCESS, CREATE_TRAVEL_SUCCESS } from '../types'
 
 const initialState = {}
 
@@ -6,6 +6,12 @@ export default function userReducer(state = initialState, action) {
    switch (action.type) {
       case GET_USER_SUCCESS: {
          return { ...action.payload }
+      }
+      case CREATE_TRAVEL_SUCCESS: {
+         return {
+            ...state,
+            travels: [...state.travels, action.payload],
+         }
       }
       default:
          return state
