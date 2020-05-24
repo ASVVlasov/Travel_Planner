176e046763742aabb55b0c7720b05c1167d83123
Elena Maltseva <lena@maltseva.design>
2020-05-24 17:51:57 +0300

    feat(TravelPage): add Loader & get for travel data

--- frontend/src/pages/TravelPage/TravelPage.jsx
+++ frontend/src/pages/TravelPage/TravelPage.jsx
@@ -1,32 +1,59 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import { bindActionCreators } from 'redux'
 import connect from 'react-redux/es/connect/connect'
 import { Route } from 'react-router-dom'
+import { getTravel } from '../../redux/travel/operations'
 
 import styles from './TravelPage.module.scss'
 import Header from '../../components/Header/Header'
 import Board from '../../components/Board/Board'
 import Footer from '../../components/Footer/Footer'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import { Loader } from '../../controls/Loader/Loader'
 
 class TravelPage extends React.Component {
+   static propTypes = {
+      getTravel: PropTypes.func.isRequired,
+      travel: PropTypes.object.isRequired,
+   }
+
+   isPropsReceived = (object) => Object.keys(object).length
+
+   componentDidMount() {
+      this.props.getTravel(this.props.match.params.travelId)
+   }
+
    render() {
-      const { path } = this.props.match
+      const {
+         match: { path },
+         travel,
+      } = this.props
+
       return (
-         <div className={styles.travelPage}>
-            <Header />
-            <Route path={`${path}/:board/:tab`} component={Board} />
-            <Footer />
-            <div className={styles.travelPage__sidebarWrap}>
-               <Route path={path} component={Sidebar} />
-            </div>
-         </div>
+         <>
+            {this.isPropsReceived(travel) ? (
+               <div className={styles.travelPage}>
+                  <Header users={travel.users} />
+                  <Route path={`${path}/:board/:tab`} component={Board} />
+                  <Footer travelId={travel.travelId} />
+                  <div className={styles.travelPage__sidebarWrap}>
+                     <Route path={path} component={Sidebar} />
+                  </div>
+               </div>
+            ) : (
+               <Loader />
+            )}
+         </>
       )
    }
 }
 
-const mapStateToProps = ({}) => ({})
-const mapDispatchToProps = (dispatch) => bindActionCreators({}, dispatch)
+const mapStateToProps = ({ travelReducer }) => ({
+   travel: travelReducer.travel,
+})
+const mapDispatchToProps = (dispatch) =>
+   bindActionCreators({ getTravel }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TravelPage)
