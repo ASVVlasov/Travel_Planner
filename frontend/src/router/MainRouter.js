a3ec31b8e0200c78d45b8decfdbc6c3dd56b417f
Constantine <buzuev.k@mail.ru>
2020-05-26 20:47:30 +0400

    feat(router): add home route

--- frontend/src/router/MainRouter.js
+++ frontend/src/router/MainRouter.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 
 import UserPage from '../pages/UserPage/UserPage'
 import TravelPage from '../pages/TravelPage/TravelPage'
+import HomePage from '../pages/HomePage/HomePage'
 
 export default class MainRouter extends React.Component {
    render() {
@@ -10,7 +11,7 @@ export default class MainRouter extends React.Component {
          <Switch>
             <Route path="/profile/:tab" component={UserPage} />
             <Route path="/travel/:travelId" component={TravelPage} />
-
+            <Route path="/home/" component={HomePage} />
             {/* // FIX Temporary redirect  */}
             <Redirect exact from="/" to="/profile/travels" />
          </Switch>
