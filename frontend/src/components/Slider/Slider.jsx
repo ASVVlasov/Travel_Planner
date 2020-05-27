bb35d586cea431fdf9348e01ea2f6c303c88f3ca
Constantine <buzuev.k@mail.ru>
2020-05-27 13:38:00 +0400

    fix(component): remaster slider

--- frontend/src/components/Slider/Slider.jsx
+++ frontend/src/components/Slider/Slider.jsx
@@ -3,61 +3,57 @@ import Swiper from 'react-id-swiper'
 import 'swiper/swiper.scss'
 import './Slider.module.scss'
 import styles from './Slider.module.scss'
-import Logotype from './Logotype.jsx'
 
 export default class Slider extends Component {
    render() {
       const params = {
-         containerClass: styles['slider-container'],
-         wrapperClass: styles['slider-wrapper'],
+         containerClass: styles['container'],
+         wrapperClass: styles['wrapper'],
          slideClass: styles.slide,
          navigation: {
-            prevEl: '.' + styles['slider-button-prev'],
-            nextEl: '.' + styles['slider-button-next'],
+            prevEl: '.' + styles['button-prev'],
+            nextEl: '.' + styles['button-next'],
          },
          pagination: {
-            el: '.' + styles['slider-pagination'],
+            el: '.' + styles['pagination'],
             type: 'bullets',
-            bulletClass: styles['slider-bullet'],
-            bulletActiveClass: styles['slider-bullet_active'],
+            bulletClass: styles['bullet'],
+            bulletActiveClass: styles['bullet_active'],
             clickable: true,
          },
          keyboard: {
             enabled: true,
             onlyInViewport: false,
          },
-         autoplay: {
-            delay: 2500,
-            disableOnInteraction: true,
-         },
+         //  autoplay: {
+         //     delay: 2500,
+         //     disableOnInteraction: true,
+         //  },
       }
       return (
-         <div className={styles.wrapper}>
-            <Logotype />
-            <Swiper {...params}>
-               <div className={styles.slide}>
-                  <div className={styles['slide-text']}>
-                     <p>Запланируйте поездку:</p>
-                     <p>для себя или вместе с друзьями</p>
-                  </div>
-                  <div className={styles['slide-image1']}></div>
+         <Swiper {...params}>
+            <div className={styles['slide']}>
+               <div className={styles['text']}>
+                  <p>Запланируйте поездку:</p>
+                  <p>для себя или вместе с друзьями</p>
                </div>
-               <div className={styles.slide}>
-                  <div className={styles['slide-text']}>
-                     <p>Соберите все документы в одном месте:</p>
-                     <p>билеты и бронирования всегда будут под рукой</p>
-                  </div>
-                  <div className={styles['slide-image2']}></div>
+               <div className={styles['image1']}></div>
+            </div>
+            <div className={styles['slide']}>
+               <div className={styles['text']}>
+                  <p>Соберите все документы в одном месте:</p>
+                  <p>билеты и бронирования всегда будут под рукой</p>
                </div>
-               <div className={styles.slide}>
-                  <div className={styles['slide-text']}>
-                     <p>В поездке останется наслаждаться</p>
-                     <p>новыми впечатлениями</p>
-                  </div>
-                  <div className={styles['slide-image3']}></div>
+               <div className={styles['image2']}></div>
+            </div>
+            <div className={styles['slide']}>
+               <div className={styles['text']}>
+                  <p>В поездке останется наслаждаться</p>
+                  <p>новыми впечатлениями</p>
                </div>
-            </Swiper>
-         </div>
+               <div className={styles['image3']}></div>
+            </div>
+         </Swiper>
       )
    }
 }
