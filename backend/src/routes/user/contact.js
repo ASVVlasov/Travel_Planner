77e132646f06cf20964cdb5dfd4deadd1c5a84c2
Alexandr <asvvlasov@gmail.com>
2020-05-30 17:06:10 +0300

    feat(contacts): add user to contacts by mail

--- backend/src/routes/user/contact.js
+++ backend/src/routes/user/contact.js
@@ -1,13 +1,22 @@
 const router = require('express').Router()
 const asyncHandler = require('express-async-handler')
 const UserModel = require('../../models/user')
+const createError = require('http-errors')
 
 router.post(
    '/',
    asyncHandler(async (req, res) => {
-      const { userId } = req.body
-      let update = { $push: { contacts: userId } }
-      res.json(await UserModel.findByIdAndUpdate(req.user._id, update, { new: true }))
+      const { email } = req.body
+      const user = await UserModel.findOne({ email })
+      if (user) {
+         if (req.user.contacts.find((c) => c.id === user.id)) {
+            throw createError(400, 'Такой пользователь уже есть в контактах')
+         }
+         const update = { $push: { contacts: user.id } }
+         res.json(await UserModel.findByIdAndUpdate(req.user._id, update, { new: true }))
+      } else {
+         throw createError(404, 'пользователь не найден')
+      }
    })
 )
 
