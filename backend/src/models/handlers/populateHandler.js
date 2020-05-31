aeedbda0e78551739bcecf9f70d3751c5342b3a7
Alexandr <asvvlasov@gmail.com>
2020-05-31 15:58:30 +0300

    feat(populate): add email and other info about user

--- backend/src/models/handlers/populateHandler.js
+++ backend/src/models/handlers/populateHandler.js
@@ -3,14 +3,14 @@ const PopulateHandler = {
       await doc
          .populate({
             path: 'users',
-            select: 'nickName avatar',
+            select: 'nickName avatar name surname middleName email',
          })
          .execPopulate()
       await doc
          .populate({
             path: 'cards',
             populate: [
-               { path: 'payers', populate: { path: 'user', select: 'nickName avatar' } },
+               { path: 'payers', populate: { path: 'user', select: 'nickName avatar name surname middleName email' } },
                { path: 'files' },
                { path: 'category' },
             ],
@@ -20,13 +20,20 @@ const PopulateHandler = {
    },
    cardToClient: async function (doc, next) {
       await doc.populate('category').execPopulate()
-      await doc.populate({ path: 'payers', populate: { path: 'user', select: 'nickName avatar' } }).execPopulate()
+      await doc
+         .populate({
+            path: 'payers',
+            populate: { path: 'user', select: 'nickName avatar name surname middleName email' },
+         })
+         .execPopulate()
       await doc.populate('files').execPopulate()
       next()
    },
    userToClient: async function (doc, next) {
       if (!!doc)
-         await doc.populate({ path: 'contacts', select: 'nickName avatar name surname middleName' }).execPopulate()
+         await doc
+            .populate({ path: 'contacts', select: 'nickName avatar name surname middleName email' })
+            .execPopulate()
       next()
    },
 }
