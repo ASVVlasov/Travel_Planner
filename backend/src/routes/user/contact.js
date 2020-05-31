713f2b52a1328f8d6783b2f1cb7b474878f80085
Alexandr <asvvlasov@gmail.com>
2020-05-31 17:41:17 +0300

    fix(contact): fix wrong logic

--- backend/src/routes/user/contact.js
+++ backend/src/routes/user/contact.js
@@ -6,14 +6,22 @@ const createError = require('http-errors')
 router.post(
    '/',
    asyncHandler(async (req, res) => {
+      const { userId } = req.body
+      const update = { $push: { contacts: userId } }
+      res.json(await UserModel.findByIdAndUpdate(req.user._id, update, { new: true }))
+   })
+)
+
+router.post(
+   '/search',
+   asyncHandler(async (req, res) => {
       const { email } = req.body
       const user = await UserModel.findOne({ email })
       if (user) {
          if (req.user.contacts.find((c) => c.id === user.id)) {
             throw createError(400, 'Такой пользователь уже есть в контактах')
          }
-         const update = { $push: { contacts: user.id } }
-         res.json(await UserModel.findByIdAndUpdate(req.user._id, update, { new: true }))
+         res.json(user)
       } else {
          throw createError(404, 'пользователь не найден')
       }
