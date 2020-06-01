76db7ce43fd774e3b60043727460ae0b3cbbb0d3
Elena Maltseva <lena@maltseva.design>
2020-06-01 23:37:55 +0300

    fix(userReducer): edit state returns & error case

--- frontend/src/redux/user/reducer.js
+++ frontend/src/redux/user/reducer.js
@@ -5,6 +5,7 @@ import {
    SEARCH_CONTACT_SUCCESS,
    UPDATE_CONTACTS_SUCCESS,
    CLEAR_CONTACTS_SEARCH,
+   FETCH_ERROR,
 } from '../types'
 
 const initialState = { newContacts: [], reqError: '' }
@@ -12,10 +13,11 @@ const initialState = { newContacts: [], reqError: '' }
 export default function userReducer(state = initialState, action) {
    switch (action.type) {
       case GET_USER_SUCCESS: {
-         return { ...action.payload }
+         return { ...state, ...action.payload }
       }
       case UPDATE_USER_SUCCESS: {
          return {
+            ...state,
             ...action.payload,
             travels: state.travels,
             contacts: state.contacts,
@@ -30,7 +32,8 @@ export default function userReducer(state = initialState, action) {
       case SEARCH_CONTACT_SUCCESS: {
          return {
             ...state,
-            newContact: action.payload,
+            newContacts: [...state.newContacts, action.payload],
+            reqError: '',
          }
       }
       case UPDATE_CONTACTS_SUCCESS: {
@@ -44,6 +47,12 @@ export default function userReducer(state = initialState, action) {
             ...state,
             newContacts: [],
             reqError: '',
+         }
+      }
+      case FETCH_ERROR: {
+         return {
+            ...state,
+            reqError: action.payload.message,
          }
       }
       default:
