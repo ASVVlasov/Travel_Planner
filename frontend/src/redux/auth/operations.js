9757a5fc49aa51f38ea46757f4383c5d83e4f2d5
Constantine <buzuev.k@mail.ru>
2020-06-04 03:54:36 +0400

    fix(auth): fix auth action/reducer

--- frontend/src/redux/auth/operations.js
+++ frontend/src/redux/auth/operations.js
@@ -1,17 +1,5 @@
-import { history } from '../store'
+import { fetchRequest } from '../fetch/operations'
+import { authorizationSuccess } from './actions'
 
-export const authorization = (authInfo, path) => async (dispatch) => {
-   console.log(authInfo)
-   const res = await fetch(path, {
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify(authInfo),
-   })
-   if (!res.ok) {
-      throw new Error(res.statusText)
-   } else {
-      history.push('/profile/travels')
-   }
-}
+export const authorization = (authInfo, path) =>
+   fetchRequest.post(path, authorizationSuccess, authInfo)
--- frontend/src/redux/user/reducer.js
+++ frontend/src/redux/user/reducer.js
@@ -7,6 +7,7 @@ import {
    UPDATE_CONTACTS_SUCCESS,
    CLEAR_CONTACTS_SEARCH,
    FETCH_ERROR,
+   AUTHORIZATION_SUCCESS,
 } from '../types'
 
 const initialState = { user: {}, newContacts: [], reqError: '' }
@@ -74,6 +75,12 @@ export default function userReducer(state = initialState, action) {
          return {
             ...state,
             reqError: action.payload.message,
+         }
+      }
+      case AUTHORIZATION_SUCCESS: {
+         return {
+            ...state,
+            reqError: '',
          }
       }
       default:
