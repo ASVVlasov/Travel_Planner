55bea311a68dd485f35ea7be0294a41aa597c97b
Elena Maltseva <lena@maltseva.design>
2020-06-05 16:26:56 +0300

    feat(MainRouter): add basic check f/authenticating

--- frontend/src/router/MainRouter.js
+++ frontend/src/router/MainRouter.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
+import connect from 'react-redux/es/connect/connect'
+
 import UserPage from '../pages/UserPage/UserPage'
 import TravelPage from '../pages/TravelPage/TravelPage'
 import HomePage from '../pages/HomePage/HomePage'
 
-export default class MainRouter extends React.Component {
+class MainRouter extends React.Component {
    render() {
+      if (!this.props.auth) {
+         return (
+            <Switch>
+               <Route path="/home/:tab" component={HomePage} />
+               <Redirect to="/home/signin" />
+            </Switch>
+         )
+      }
       return (
          <Switch>
             <Route path="/profile/:tab" component={UserPage} />
             <Route path="/travel/:travelId" component={TravelPage} />
-            <Route path="/home/:tab" component={HomePage} />
-            {/* // FIX Temporary redirect  */}
-            <Redirect exact from="/" to="/home/signin" />
+            <Redirect to="/profile/travels" />
          </Switch>
       )
    }
 }
+
+const mapStateToProps = ({ userReducer }) => ({
+   auth: userReducer.auth,
+})
+export default connect(mapStateToProps)(MainRouter)
