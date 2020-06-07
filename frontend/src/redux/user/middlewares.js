03cec690a0221938732d5ee52d2f621ae9319ecf
Discipulus101 <Discipulus101@users.noreply.github.com>
2020-06-07 14:05:41 +0300

    fix(user\middlewares): travel sorting code restructuring

--- frontend/src/redux/user/middlewares.js
+++ frontend/src/redux/user/middlewares.js
@@ -1,40 +1,22 @@
 import { GET_USER_SUCCESS, CREATE_TRAVEL_SUCCESS } from '../types'
 import { getTravelsFilter } from './actions'
 
+function sortByTime(travels) {
+   travels.sort(function (prev, next) {
+      const beginD = new Date(prev.beginDate).setHours(0, 0, 0, 0)
+      const endD = new Date(next.beginDate).setHours(0, 0, 0, 0)
+      const beginDiff = beginD - endD
+      const endDiff = new Date(prev.endDate) - new Date(next.endDate)
+      return beginDiff > 0 ? 1 : beginDiff < 0 ? -1 : endDiff > 0 ? 1 : -1
+   })
+}
+
 export default (store) => (next) => (action) => {
    next(action)
 
    switch (action.type) {
       case GET_USER_SUCCESS:
       case CREATE_TRAVEL_SUCCESS: {
-         function sortByTime(travels) {
-            travels.sort(function (prev, next) {
-               let a = Math.round(
-                  new Date(prev.beginDate) / (24 * 60 * 60 * 1000) //start date of trip as number of days from 1970.
-               )
-               let b = Math.round(
-                  new Date(next.beginDate) / (24 * 60 * 60 * 1000)
-               )
-
-               if (a > b) {
-                  return 1
-               }
-               if (
-                  //if there are trips starting on the same day, they are compared by end date
-                  a === b &&
-                  new Date(prev.endDate).getDate() <
-                     new Date(next.endDate).getDate()
-               ) {
-                  return -1
-               }
-               if (a < b) {
-                  return -1
-               }
-               return 0
-            })
-            return travels
-         }
-
          let sortTravels = store.getState().userReducer.user.travels
 
          let activeTravels = sortTravels.filter(
