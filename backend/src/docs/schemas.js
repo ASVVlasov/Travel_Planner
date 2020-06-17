9b6bd4bbb502c3738d2e19463f72b9da641943d0
Constantine <buzuev.k@mail.ru>
2020-06-17 19:47:42 +0400

    feat(swagger): add feedback to swagger docs

--- backend/src/docs/schemas.js
+++ backend/src/docs/schemas.js
@@ -7,6 +7,7 @@ const Card = require('../models/card.js')
 const File = require('../models/file.js')
 const Category = require('../models/category.js')
 const Payer = require('../models/payer.js')
+const Feedback = require('../models/feedback.js')
 /* Documents */
 const UserSchema = m2s(User)
 const TravelSchema = m2s(Travel)
@@ -14,6 +15,7 @@ const CardSchema = m2s(Card)
 const PayerSchema = m2s(Payer)
 const FileSchema = m2s(File)
 const CategorySchema = m2s(Category)
+const FeedbackSchema = m2s(Feedback)
 /* Combining schema documents in one */
 const schemas = {}
 schemas.User = UserSchema
@@ -22,5 +24,6 @@ schemas.Card = CardSchema
 schemas.Payer = PayerSchema
 schemas.File = FileSchema
 schemas.Category = CategorySchema
+schemas.Feedback = FeedbackSchema
 
 module.exports = schemas
