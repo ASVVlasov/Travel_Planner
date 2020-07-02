b3e09ce515d2fec6d2de05a78fdbf471e219056b
Constantine <buzuev.k@mail.ru>
2020-07-02 21:11:53 +0400

    fix(route/feedback): fix after review

--- backend/src/routes/feedback/index.js
+++ backend/src/routes/feedback/index.js
@@ -6,9 +6,7 @@ router.post(
    '/',
    asyncHandler(async (req, res) => {
       const feedback = { ...req.body }
-      feedback.user = {}
       feedback.user = req.user._id
-      feedback.date = {}
       feedback.date = new Date()
       const newFeedback = await FeedbackModel.create(feedback)
       res.json(newFeedback)
